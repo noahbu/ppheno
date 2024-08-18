c3791e80cf965da3cb6bf6dcfc294d4408e9f127
Noah Bucher <noah-bucher@mailbox.org>
2024-08-18 22:00:38 +0800

    added k-means clustering based on colour and spatial information. Also added GMM clustering, which leads to god results when only considering spatial information.

--- scripts/pc_processing/colour_histogram.py
+++ scripts/pc_processing/colour_histogram.py
@@ -1,9 +1,22 @@
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+# Get the current script's directory
+script_dir = Path(__file__).parent.resolve()
+
+# Construct the path to the root of the GitHub project
+project_root = script_dir.parent.parent
+
+# Construct the path to the /data folder
+data_folder = project_root / Path('data/melonCycle/2024-07-30/B-4')
+
+# Paths to the files
+point_cloud_file = data_folder / 'filtered_point_cloud2.ply'
 
 # Load your cleaned point cloud
-pcd = o3d.io.read_point_cloud("/Users/noahbucher/Documents_local/Plant_reconstruction/melonCycle/2024-07-30/A-1/point_cloud_edits/cleaned_A-1.ply")
+pcd = o3d.io.read_point_cloud(str(point_cloud_file))
 
 # Convert to NumPy array
 colors = np.asarray(pcd.colors)
